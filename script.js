2fb9722b3a434d9bce9efe5c5f2eb28d74377ab7
Lockop1 <anthonynewsome8@gmail.com>
2025-04-15 16:02:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.get('scroll') === 'half') {
-    const halfway = document.body.scrollHeight / 2.5;
+    const halfway = document.body.scrollHeight / 3.15;
     window.scrollTo({
       top: halfway,
       behavior: 'smooth'
