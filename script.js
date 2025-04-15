d29004fb0459056e0296426b7516aad6350f6bd3
Lockop1 <anthonynewsome8@gmail.com>
2025-04-15 15:57:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.get('scroll') === 'half') {
-    const halfway = document.body.scrollHeight / 1.75;
+    const halfway = document.body.scrollHeight / 2.5;
     window.scrollTo({
       top: halfway,
       behavior: 'smooth'
@@ -9,7 +9,7 @@ window.addEventListener('load', function () {
   }
 
   if (urlParams.get('scroll') === 'full') {
-    const halfway = document.body.scrollHeight;
+    const halfway = document.body.scrollHeight / 1.15;
     window.scrollTo({
       top: halfway,
       behavior: 'smooth'
